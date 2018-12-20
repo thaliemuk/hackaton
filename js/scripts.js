c17065b4e80969d0690badc540c54eafc910fa34
thalie.mukhtar <thalie.mukhtar@timetoknow.com>
2018-12-20 14:50:15 +0200

    hackaton

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
+function goToPage(pageNumber) {
+    let prevPage = pageNumber - 1;
+    if (prevPage === 0) {
+        prevPage = document.getElementsByClassName("nano-time").length;
+    }
+    document.querySelector(".nano-time.page-" + prevPage).classList.add("hidden");
+    document.querySelector(".nano-time.page-" + pageNumber).classList.remove("hidden");
+}
 
-function goToPage(pageNumber){
-	let prevPage = pageNumber - 1;
-	if(prevPage === 0){
-		prevPage = document.querySelector(".nano-time").length;
-	}
-	document.querySelector(".nano-time.page-"+prevPage).classList.add("hidden");
-	document.querySelector(".nano-time.page-"+pageNumber).classList.remove("hidden");
+function setSelected(e) {
+    let selected = document.querySelector(".answer.selected");
+    if (selected) {
+        selected.classList.remove("selected");
+    }
+    e.classList.add("selected");
 }
