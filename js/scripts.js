866f60fcc28a0407d2530f1953fc5e62dd055014
thalie.mukhtar <thalie.mukhtar@timetoknow.com>
2018-12-20 13:57:00 +0200

    hackaton

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 
 function goToPage(pageNumber){
 	let prevPage = pageNumber - 1;
+	if(prevPage === 0){
+		prevPage = document.querySelector(".nano-time").length;
+	}
 	document.querySelector(".nano-time.page-"+prevPage).classList.add("hidden");
 	document.querySelector(".nano-time.page-"+pageNumber).classList.remove("hidden");
 }
